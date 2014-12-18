8731f31326d862c867552ba41d940004c8eb53de
Steven Perez <steven.gpa@gmail.com>
2014-12-18 08:52:56 -0600

    Update Objects Done, return null was changed for throw exceptions

--- dist/road.js
+++ dist/road.js
@@ -3,7 +3,8 @@ var road = function road()
 	var data = [];		// Object container of the information
 	var idCounter = 0;	// Avoid CID duplicates
 	
-	function genID()	// Generate Cache ID at the moment to Add or Set new data
+	// Generate Cache ID at the moment to Add or Set new data
+	function genID()	
 	{
 		try
 		{
@@ -26,7 +27,26 @@ var road = function road()
 			return '';
 		}
 	}
-		
+	
+	// Update the Destination property's values base on the Source
+	// when key properties match
+	function updateProperties(Source, Destination)
+	{
+		try {
+			
+			for (var key in Source) {
+
+				if(Destination.hasOwnProperty(key)){
+					Destination[key] = Source[key];
+				}
+				
+			}
+		}
+		catch (err)
+		{ throw err; }
+			
+	}
+	
 	return {
 		
 		add: function add(obj, fun) {
@@ -41,7 +61,7 @@ var road = function road()
 					length = obj.length || 1;
 				else
 				// If object was not passed
-					return null;
+					throw 'Missing object to be included';
 				
 				// If it is an Array of Objects
 				if (length > 1)
@@ -92,10 +112,10 @@ var road = function road()
 			{
 				// Check if a cid was not passed
 				if (!cid || this.length() == 0)
-					return null;
+					throw 'Missing cid or No data in memory';
 				
-				var currentData = data;
-				var removeIndex = data.map(function(currentData) { return currentData.cid; }).indexOf(cid);
+				// Get the index of the item to delete
+				var removeIndex = data.map(function(data) { return data.cid; }).indexOf(cid);
 				
 				// If an object was found
 				if (removeIndex >= 0) 
@@ -104,6 +124,75 @@ var road = function road()
 					return null;
 			}
 			catch(err)
+			{ throw err; }
+		},
+		
+		update: function update(obj, cid, fun) {
+			try {
+				// Validations
+				if (!obj)
+					throw 'Missing object';
+								
+				if (cid)
+				{
+					if (typeof(cid) == 'number')
+						throw 'cid value should be String';
+				}
+				
+				var length = 0;
+				// If the object was passed
+				if (obj) 
+					length = obj.length || 1;
+				else
+				// If object was not passed
+					throw 'Missing object to be included';
+				
+				// If it is an Array of Objects to be updated
+				if (length > 1)
+				{
+					// Loop and add the properties to each Object
+					for (var i = 0; i < length; i++)
+					{
+						if (!obj[i].cid)
+							throw 'For array updates it is required the cid property on each object.';
+						// Get the object from Data by cid
+						var objData = this.getByCID(obj[i].cid, false);
+						
+						// Validate if item was found
+						if (objData == undefined)
+							throw 'No item found for cid ' + cid.toString();
+						
+						// Update the properties between Source and Destination
+						updateProperties(obj[i], objData);
+						objData.status = 'changed';
+					}
+				}
+				else
+				// If it is just one Object
+				{
+					if (!obj.cid && !cid)
+						throw 'Missing cid';
+				
+					if (!cid && obj.cid)
+						cid = cid.toString() || obj.cid.toString();
+					
+					// Get the object from Data by cid
+					var objData = this.getByCID(cid.toString(), false);
+					
+					// Validate if item was found
+					if (objData == undefined)
+						throw 'No item found for cid ' + cid.toString();
+					
+					// Update the properties between Source and Destination
+					updateProperties(obj, objData);
+					
+					// Change Status and reinforce the cid item
+					objData.cid = cid.toString();
+					objData.status = 'changed';
+				}
+				
+			}
+			catch (err)
 			{ throw err; }
 		},
 				
@@ -125,18 +214,38 @@ var road = function road()
 			{ throw err; }
 		},
 		
-		getByCID: function getByCID(cid) {
+		getByCID: function getByCID(cid, isClone) {
 			try
-			{
-				if (!cid)
-					return null;
+			{	
+				// If it is undefined then create a Clone of the
+				// returned object
+				if (isClone == undefined) { isClone = true; }
 				
-				return Enumerable.From(data).Where(
+				// Validate cid
+				if (!cid)
+					throw 'Missing cid';
+				
+				// If it is necessary to return a new object
+				if (isClone)
+				{
+					return Object.create(Enumerable.From(data).Where(
+													function (x) { 
+														return x.cid.toString() == cid.toString() 
+													})
+											.Select()
+											.ToArray()[0]);
+				}
+				else
+				// If not, then work with the object linked
+				{
+					return Enumerable.From(data).Where(
 													function (x) { 
 														return x.cid.toString() == cid.toString() 
 													})
 											.Select()
 											.ToArray()[0];
+				}
+				
 			}
 			catch(err) { return err; }
 		},
@@ -152,3 +261,8 @@ var road = function road()
 	};
 	
 };
+
+var a = road();
+a.add({ name: 'Steven', age: 20});
+a.add({ name: 'Mochi', age: 21});
+a.add({ name: 'Marta', age: 22});
